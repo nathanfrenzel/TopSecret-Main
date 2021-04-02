d197953830fa1912d70444483e3c8be8173f9f86
Bruce Blake <bruceiiiblake@gmail.com>
2021-04-02 19:04:19 -0400

    Initial Commit

---
+++ TopSecret/TopSecretApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopSecretApp.swift
+//  TopSecret
+//
+//  Created by Bruce Blake on 4/2/21.
+//
+
+import SwiftUI
+
+@main
+struct TopSecretApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
